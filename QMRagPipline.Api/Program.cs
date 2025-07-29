af18df3305528994a6714339e177ae3664860487
Yirmiyahu Tesler <yermi72@gmail.com>
2025-07-29 03:15:39 +0300

    fix of using settings

--- QMRagPipline.Api/Program.cs
+++ QMRagPipline.Api/Program.cs
@@ -6,9 +6,12 @@ using QMRagPipeline.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
